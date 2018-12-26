596a24157c4640d90dd54c434bc6edfc5c9a1bbd
Kosta_ Eric 561/2015 <kgkosta360@gmail.com>
2018-12-26 12:47:05 +0100

    Add files via upload

--- izmeni.php
+++ izmeni.php
@@ -12,11 +12,47 @@
 		<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
 		<link rel="stylesheet" href="prikaz.css">
+		<link rel="stylesheet" href="navbar.css">
 		
 	</head>
 
 	<body>
-		<div class="container">
+		<nav class="navbar sticky-top navbar-expand-lg">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="admin.php">Home </a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="dodaj1.php">Dodavanje</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="prikaz.php">Prikaz</a>
+				</li>
+
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Dropdown
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="#">Action</a>
+					<a class="dropdown-item" href="#">Another action</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Something else here</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#">Disabled</a>
+				</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="container izmeni">
 			<div class="row">
 				<div class="col-sm-4">
 					<h1>Izmena</h1>
--- prikaz.php
+++ prikaz.php
@@ -1,6 +1,10 @@
 <html>
     <head>
 
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    
+
 		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -13,32 +17,47 @@
 		
 
         
-        <link rel="stylesheet" href="prikaz.css">
+		<link rel="stylesheet" href="prikaz.css">
+		<link rel="stylesheet" href="navbar.css">
 			
     </head>
 
 	<body>
-        <div id="head">
-			<div class="row">
-				<div class="col-sm-7">           
-					<h2>Proizvodi</h2>
-				</div>
-				<div class="col-sm-5">
-					<div class="search">						                  
-						<form id="form0" action="prikaz.php" method="GET">
-							<select name="prikaz" onchange="submitForm0()" class="form-control">
-								<option selected hidden disabled>Prikazi po</option>
-								<option>Imenu</option>
-								<option>Vrsti</option>
-								<option>Proizvodjacu</option>
-							</select>
-							<button><i class="glyphicon glyphicon-arrow-up"></i></button>
-							<button><i class="glyphicon glyphicon-arrow-down"></i></button>
-						</form>						    
-					</div> 
-				</div> 
-			</div>	               	
-        </div>
+		<nav class="navbar sticky-top navbar-expand-lg">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="admin.php">Home </a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="dodaj1.php">Dodavanje</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="prikaz.php">Prikaz</a>
+				</li>
+
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Dropdown
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+					<a class="dropdown-item" href="#">Action</a>
+					<a class="dropdown-item" href="#">Another action</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Something else here</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#">Disabled</a>
+				</li>
+				</ul>
+			</div>
+		</nav>
 		<div class="config">								
 			<div class="container big">
                 <div class="row">
