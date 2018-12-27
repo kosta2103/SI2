bd91e713a4493981f151bb545dc02720a6a9ed4e
Nevena <557.2015@fink.rs>
2018-12-27 01:06:41 +0100

    Add files via upload

---
+++ otvaranje.php
@@ -0,0 +1,13 @@
+
+
+
+<?php
+
+$imefajla = $_POST['fajl'];
+ 
+   header('Content-Disposition: inline; filename="'.$imefajla.'"');
+      header('Content-Type: application/pdf');
+      header('Content-Length: '.filesize($imefajla));
+      readfile($imefajla);
+
+?>
