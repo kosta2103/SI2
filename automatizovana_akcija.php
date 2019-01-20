1edd4d254fdc86c196bc27e06e36c80bec466624
aleksandraradovic5512015 <radovic.a96@gmail.com>
2019-01-20 19:13:14 +0100

    Add files via upload

---
+++ automatizovana_akcija.php
@@ -0,0 +1,26 @@
+<?php
+$konekcija = mysqli_connect("localhost", "root", "", "SI2");
+
+if(isset($_GET["potvrda"])){
+    $barkod = $_GET["barkod"];
+    $cena = $_GET["cena"]; 
+    $procenat = $_GET["procenat"];
+    $radnja = $_GET["radnja"];
+
+    $sql = "UPDATE proizvodi SET Cena = '".$cena."' WHERE Barcode = '".$barkod."'";
+    $konekcija->query($sql) or die($konekcija->error);
+
+    if($radnja == "upis"){
+        $sql = "INSERT INTO snizenje (Vrsta, Naziv, Procenat, Vazi) VALUES ('Slaba_prodaja', '".$barkod."', '".$procenat."', TRUE)";
+        $konekcija->query($sql) or die($konekcija->error);
+    }
+    else{
+        $sql = "UPDATE snizenje SET Procenat = '".$procenat."' WHERE Naziv = '".$barkod."'";   
+        $konekcija->query($sql) or die($konekcija->error);
+    }
+}
+echo $barkod;
+?>
+<script>
+        window.location.href = 'admin.php';
+</script>
