1f76c0afabbfa56805300b60a18248f8115dd833
aleksandraradovic5512015 <radovic.a96@gmail.com>
2019-01-23 01:05:39 +0100

    Update dodaj2.php

--- dodaj2.php
+++ dodaj2.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 if(isset($_SESSION['pristup']) && ($_SESSION['pristup'] == "Administrator" || $_SESSION['pristup'] == "Vlasnik" || $_SESSION['pristup'] == "Komercijalista")){
 
 $veza = mysqli_connect("localhost", "root", "", "SI2");
